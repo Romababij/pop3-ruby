1f09451972356b504945e8b7c1aecfbee7829c10
Romababij <rba@jetthoughts.com>
2017-03-23 19:26:18 +0200

    Added pop3 file.

---
+++ pop3.rb
@@ -0,0 +1,26 @@
+require 'net/pop'
+
+user_name = '***'
+user_password = '***'
+pop3_server = 'pop.mail.ru'
+
+Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
+
+p_server = Net::POP3.new(pop3_server, 995)
+
+p_server.start(user_name, user_password)
+
+if p_server.mails.empty?
+  p 'No mail.'
+else
+  p "#{p_server.mails.size} mails in box."
+
+  p_server.mails.each_with_index do |m, index|
+    if index == p_server.mails.size - 1
+      p m.header.match(/^From: ([^\s]+)/)[1]
+    end
+  end
+
+end
+
+p_server.finish
